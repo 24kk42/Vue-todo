f656bb9de4e6dfbbb4713d1afc0a77528ff203f1
Fatih Guzel <fatihguzel2002@hotmail.com>
2022-12-13 11:59:53 +0300

    Todo item created

---
+++ src/components/todo-item.vue
@@ -0,0 +1,3 @@
+<template src="./todo-item.html" />
+<script lang="ts" src="./todo-item.ts" />
+<style src="./todo-item.scss" lang="scss" />
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueApollo from 'vue-apollo'
+import apolloClient from './apollo/apollo'
 
 Vue.config.productionTip = false
 
+Vue.use(VueApollo)
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+})
+
 new Vue({
+  apolloProvider,
   render: h => h(App),
 }).$mount('#app')
